a93ffcc55e034cb373ee83c84dc44af89e712971
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-03-24 16:36:01 +0100

    proof of randomized quicksort

--- engine.js
+++ engine.js
@@ -3,6 +3,7 @@ function show(obj) {
 }
 
 window.onload = () => {
+
     console.log("loading...");
     let sprites = document.getElementsByTagName("sprite");
     for (let i = 0; i < sprites.length; i++) {
@@ -12,11 +13,26 @@ window.onload = () => {
         sprite.appendChild(img);
         sprite.style.left = sprite.getAttribute("x") + "px";
         sprite.style.top = sprite.getAttribute("y") + "px";
-        
-        
+    }
+
+    let infers = document.getElementsByTagName("infer");
+    for (let i = 0; i < infers.length; i++) {
+        const el = infers[i];
+        el.onclick = () => {
+            const txt = el.getAttribute("speech");
+            if (txt != null)
+                say(txt);
+        };
     }
 
 
+}
+
+
+function say(str) {
+    const utterance = new SpeechSynthesisUtterance(str);
+    utterance.lang = "fr";
+    speechSynthesis.speak(utterance);
 }
 
 
