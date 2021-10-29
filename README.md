335b00e07ced8a4364f3dd9b4a1e6a44342024b5
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-10-29 15:16:40 +0200

    restructuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# click-and-prove
+# Click-and-prove
+
+This project is for transforming a proof tree written in LaTEX in an interactive game.
--- engine.js
+++ engine.js
@@ -1,19 +1,114 @@
-function show(obj) {
-    obj.style.visibility = "visible";
+class ProofNode {
+    conclusion;
+    premisses = [];
+
+    constructor(conclusion) { this.conclusion = conclusion; }
+    addPremisse(premisse) { this.premisses.push(premisse); }
+}
+
+
+function extractContent(line) {
+    const i = line.indexOf("{");
+    const j = line.lastIndexOf("}");
+    return dollarToBackSlashParenthesis(line.substr(i + 1, j - i - 1));
+}
+
+
+function dollarToBackSlashParenthesis(line) {
+    let left = true;
+    while (line.indexOf("$") > -1) {
+        line = line.replace("$", left ? "\\(" : "\\)");
+        left = !left;
+    }
+    return line;
+}
+
+
+function testExist(obj) {
+    if (obj == undefined)
+        throw "error"
+    return obj;
+}
+
+
+
+function textToProof(text) {
+    let nodes = [];
+    for (const line of text.split("\n").map((line) => line.trim())) {
+        console.log(line);
+        if (line.startsWith("\\AxiomC")) {
+            nodes.push(new ProofNode(extractContent(line)));
+        }
+        else if (line.startsWith("\\UnaryInfC")) {
+            const node = new ProofNode(extractContent(line));
+            node.addPremisse(testExist(nodes.pop()));
+            nodes.push(node);
+        }
+        if (line.startsWith("\\BinaryInfC")) {
+            const node = new ProofNode(extractContent(line));
+            node.addPremisse(testExist(nodes.pop()));
+            node.addPremisse(testExist(nodes.pop()));
+            nodes.push(node);
+        }
+        if (line.startsWith("\\TernaryInfC")) {
+            const node = new ProofNode(extractContent(line));
+            node.addPremisse(testExist(nodes.pop()));
+            node.addPremisse(testExist(nodes.pop()));
+            node.addPremisse(testExist(nodes.pop()));
+            nodes.push(node);
+        }
+    }
+    const node = testExist(nodes.pop())
+    console.log(node)
+    return node;
+}
+
+
+
+
+function proofToDOM(node) {
+    const element = document.createElement("proof");
+
+    if (node.premisses.length >= 1) {
+        const elementPremisses = document.createElement("proofs");
+        for (const child of node.premisses) {
+            elementPremisses.appendChild(proofToDOM(child));
+        }
+        element.appendChild(elementPremisses);
+    }
+
+    const elementInfer = document.createElement("infer");
+    elementInfer.innerHTML = node.conclusion;
+    element.appendChild(elementInfer);
+    return element;
+}
+
+
+async function load(filename) {
+    const response = await fetch(`proofs/${filename}.tex`);
+    const text = await response.text();
+    const textProofs = text.split("\\end{prooftree}");
+    for (let i = 0; i < textProofs.length - 1; i++) {
+        const textProof = textProofs[i];
+        const node = textToProof(textProof);
+
+        const button = document.createElement("button");
+        button.innerHTML = node.conclusion;
+        button.onclick = () => {
+            document.getElementById("proof").innerHTML = "";
+            document.getElementById("proof").appendChild(proofToDOM(node));
+            MathJax.typeset();
+        }
+
+        document.getElementById("menu").appendChild(button);
+    }
+
+    MathJax.typeset();
 }
 
 window.onload = () => {
-
-    console.log("loading...");
-    let sprites = document.getElementsByTagName("sprite");
-    for (let i = 0; i < sprites.length; i++) {
-        const sprite = sprites[i];
-        const img = new Image();
-        img.src = `img/${sprite.id}.png`;
-        sprite.appendChild(img);
-        sprite.style.left = sprite.getAttribute("x") + "px";
-        sprite.style.top = sprite.getAttribute("y") + "px";
-    }
+    //load("bidirectional");
+    load("sqrt2irrationnal");
 
     let infers = document.getElementsByTagName("infer");
     for (let i = 0; i < infers.length; i++) {
@@ -37,10 +132,3 @@ function say(str) {
 
 
 
-function goto(obj) {
-    let scenes = document.getElementsByTagName("scene");
-    for (let i = 0; i < scenes.length; i++) {
-        scenes[i].style.visibility = "hidden";
-    }
-    obj.style.visibility = "visible";
-}
