946df2a3925cbbdd5db857976245d347aea451fa
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-10-31 14:39:41 +0100

    restructuration

--- engine.js
+++ engine.js
@@ -4,13 +4,61 @@ class ProofNode {
 
     constructor(conclusion) { this.conclusion = conclusion; }
     addPremisse(premisse) { this.premisses.push(premisse); }
+
+    toDOM() {
+        const element = document.createElement("proof");
+
+        if (this.premisses.length >= 1) {
+            const elementPremisses = document.createElement("proofs");
+            for (const child of this.premisses) {
+                elementPremisses.appendChild(child.toDOM());
+            }
+            element.appendChild(elementPremisses);
+        }
+
+        const elementInfer = document.createElement("infer");
+        elementInfer.innerHTML = this.conclusion;
+        element.appendChild(elementInfer);
+        return element;
+    }
+}
+
+
+class Assumption {
+    constructor(assumption) { this.assumption = assumption; }
+
+    toDOM() {
+        const element = document.createElement("assumption");
+        element.innerHTML = this.assumption;
+        return element;
+    }
+}
+
+
+class Sequence {
+    content = [];
+    addContent(content) { this.content.push(...content); }
+
+    constructor(content) {
+        this.content = content;
+    }
+
+    toDOM() {
+        const element = document.createElement("content");
+        for (const child of this.content) {
+            element.appendChild(child.toDOM());
+        }
+        return element;
+    }
 }
 
 
 function extractContent(line) {
     const i = line.indexOf("{");
     const j = line.lastIndexOf("}");
-    return dollarToBackSlashParenthesis(line.substr(i + 1, j - i - 1));
+    const text = dollarToBackSlashParenthesis(line.substr(i + 1, j - i - 1));
+    console.log("extract text = " + text);
+    return text;
 }
 
 
@@ -26,89 +74,76 @@ function dollarToBackSlashParenthesis(line) {
 
 function testExist(obj) {
     if (obj == undefined)
-        throw "error"
+        throw "error, we popped an element and there should be someone"
     return obj;
 }
 
 
 
-function textToProof(text) {
+function linesToProof(lines) {
     let nodes = [];
-    for (const line of text.split("\n").map((line) => line.trim())) {
+    while (lines.length > 0) {
+        const line = lines.shift();
         console.log(line);
-        if (line.startsWith("\\AxiomC")) {
+        if (line == "{") {
+            nodes.push(linesToProof(lines));
+        }
+        else if (line == "}") {
+            return new Sequence(nodes);
+        }
+        else if (line.startsWith("\\AxiomC")) {
             nodes.push(new ProofNode(extractContent(line)));
+        }
+        if (line.startsWith("\\Assume")) {
+            nodes.push(new Assumption(extractContent(line)));
         }
         else if (line.startsWith("\\UnaryInfC")) {
             const node = new ProofNode(extractContent(line));
             node.addPremisse(testExist(nodes.pop()));
             nodes.push(node);
         }
-        if (line.startsWith("\\BinaryInfC")) {
+        else if (line.startsWith("\\BinaryInfC")) {
             const node = new ProofNode(extractContent(line));
             node.addPremisse(testExist(nodes.pop()));
             node.addPremisse(testExist(nodes.pop()));
             nodes.push(node);
         }
-        if (line.startsWith("\\TernaryInfC")) {
+        else if (line.startsWith("\\TrinaryInfC")) {
             const node = new ProofNode(extractContent(line));
             node.addPremisse(testExist(nodes.pop()));
             node.addPremisse(testExist(nodes.pop()));
             node.addPremisse(testExist(nodes.pop()));
             nodes.push(node);
         }
+        console.log("nb of nodes: " + nodes.length);
     }
-    const node = testExist(nodes.pop())
-    console.log(node)
-    return node;
+    if (nodes.length == 0)
+        throw "error";
+    else if (nodes.length == 1)
+        return nodes[0];
+    else
+        return new Sequence(nodes);
 }
 
 
 
 
-function proofToDOM(node) {
-    const element = document.createElement("proof");
-
-    if (node.premisses.length >= 1) {
-        const elementPremisses = document.createElement("proofs");
-        for (const child of node.premisses) {
-            elementPremisses.appendChild(proofToDOM(child));
-        }
-        element.appendChild(elementPremisses);
-    }
-
-    const elementInfer = document.createElement("infer");
-    elementInfer.innerHTML = node.conclusion;
-    element.appendChild(elementInfer);
-    return element;
-}
 
 
 async function load(filename) {
-    const response = await fetch(`proofs/${filename}.tex`);
+    const response = await fetch(`proofs/${filename}.proof`);
     const text = await response.text();
-    const textProofs = text.split("\\end{prooftree}");
-    for (let i = 0; i < textProofs.length - 1; i++) {
-        const textProof = textProofs[i];
-        const node = textToProof(textProof);
 
-        const button = document.createElement("button");
-        button.innerHTML = node.conclusion;
-        button.onclick = () => {
-            document.getElementById("proof").innerHTML = "";
-            document.getElementById("proof").appendChild(proofToDOM(node));
-            MathJax.typeset();
-        }
-
-        document.getElementById("menu").appendChild(button);
-    }
-
+    const proof = linesToProof(text.split("\n").map((line) => line.trim()));
+    document.getElementById("proof").appendChild(proof.toDOM());
     MathJax.typeset();
 }
 
 window.onload = () => {
     //load("bidirectional");
     load("sqrt2irrationnal");
+    // load("karp-lipton");
+
 
     let infers = document.getElementsByTagName("infer");
     for (let i = 0; i < infers.length; i++) {
